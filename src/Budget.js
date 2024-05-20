63253002caaa0962673cbe5919ee510c45a71b5c
Chakradar Raju <chakradarraju@gmail.com>
2024-05-20 22:34:33 +0530

    Making page responsive

--- src/Budget.js
+++ src/Budget.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { PieChart, Pie, Cell, Tooltip } from 'recharts';
+import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer } from 'recharts';
 import { budgetData } from './budgetData';
 import { Grid } from '@mui/material';
 
@@ -22,7 +22,7 @@ const Budget = () => {
     <div>
       <h1>Budget 2024</h1>
       <Grid container style={{ flexWrap: 'wrap' }}>
-        <Grid item xs={6}>
+        <Grid item xs={12} sm={8}>
           <table>
             <thead>
               <tr>
@@ -40,32 +40,34 @@ const Budget = () => {
             </tbody>
           </table>
         </Grid>
-        <Grid item xs={6}>
-          <PieChart width={800} height={400}>
-            <Pie
-              data={pieData}
-              cx={400}
-              cy={200}
-              labelLine={false}
-              outerRadius={150}
-              startAngle={90}
-              endAngle={-270}
-              isClockwise
-              fill="#8884d8"
-              dataKey="value"
-              label={false}
-            >
-              {pieData.map((entry, index) => (
-                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-              ))}
-            </Pie>
-            <Tooltip 
-              formatter={(value, name, props) => {
-                const percentage = ((value / totalExpenditure) * 100).toFixed(2);
-                return [`₹${value.toLocaleString()} (${percentage}%)`, name];
-              }}
-            />
-          </PieChart>
+        <Grid item xs={12} sm={4}>
+          <ResponsiveContainer height={400}>
+            <PieChart>
+              <Pie
+                data={pieData}
+                cx="50%"
+                cy="50%"
+                labelLine={false}
+                outerRadius="85%"
+                startAngle={90}
+                endAngle={-270}
+                isClockwise
+                fill="#8884d8"
+                dataKey="value"
+                label={false}
+              >
+                {pieData.map((entry, index) => (
+                  <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                ))}
+              </Pie>
+              <Tooltip 
+                formatter={(value, name, props) => {
+                  const percentage = ((value / totalExpenditure) * 100).toFixed(2);
+                  return [`₹${value.toLocaleString()} (${percentage}%)`, name];
+                }}
+              />
+            </PieChart>
+          </ResponsiveContainer>
           <span>
             Source: <a href="https://www.indiabudget.gov.in/doc/eb/allsbe.pdf">Expenditure Budget 2024-25</a>
           </span>
